34ce1954f679cae32e426e8df360b0a53e9e76ed
primelt-software <110195598+primelt-software@users.noreply.github.com>
2025-10-06 20:37:34 -0700

    Update AFC.py

--- src/PetThermoTools/AFC.py
+++ src/PetThermoTools/AFC.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from PetThermoTools.MELTS import MELTSdynamic
 
 # ------------------------------------------------------------------------------------
 # --- Helper functions ---------------------------------------------------------------
